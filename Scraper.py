f7a77f69677b5d629293c31ac6b58876ea1bc8c0
paxip <109951080+paxip@users.noreply.github.com>
2022-09-29 09:49:18 +0100

    Load webpage

--- Scraper.py
+++ Scraper.py
@@ -15,6 +15,39 @@ class Scraper:
         driver.get(URL)
         time.sleep(3)
 
+        tab_container = driver.find_element(by=By.XPATH, value='//*[@id="a-page"]/div[2]/div[4]/div') 
+        monthly_button = tab_container.find_element(by=By.XPATH, value='//*[@id="a-page"]/div[2]/div[4]/div/a[4]') 
+        monthly_button.click()
+        time.sleep(3)
+
+        drop_down_menu = driver.find_element(By.CLASS_NAME, 'a-dropdown-container')
+        inner_drop_down_button = drop_down_menu.find_element(By.CLASS_NAME, 'a-button-inner')
+        inner_drop_down_button.click()
+
+        
+        
+        #a_tag = driver.find_element(by=By.TAG_NAME, value='a')
+        #by_year_button = drop_down_menu.find_element(By.CLASS_NAME, "a-dropdown-link")
+        #by_year_button.click()
 
 
         
+
+        
+
+        
+Scraper.load_webpage()
+
+
+
+        
+
+
+
+
+
+   
+
+
+
+
