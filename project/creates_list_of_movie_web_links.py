41a30b9a4cfa597073edb58f17149e7c23c416a3
paxip <109951080+paxip@users.noreply.github.com>
2022-11-15 17:45:36 +0000

    Displays docstrings for first scraper class

--- project/creates_list_of_movie_web_links.py
+++ project/creates_list_of_movie_web_links.py
@@ -12,8 +12,38 @@ import requests
 import time
 
 
-class Web_link_scraper:    
-    def __init__(self, url: str="https://www.boxofficemojo.com/", driverpath: str='/Applications/chromedriver'):     
+class Web_link_scraper:
+    '''
+    This class scrapes a list of links from the IMDB website for a list of years that you specify and 
+    creates a list of movie links.
+    
+    Attributes:
+    ----------
+    service : class
+        Manages the starting and stopping of the ChromeDriver.
+    options : class
+        Used to create an instance of the chrome driver.
+    driver : object
+        Gets the url.
+    movie_link_list : list
+        Creates a list of web elements corresponding to each movie from the IMDB website.
+    category_heading_list : list
+        Creates a list of all the column headings from the table of information for each movie link.
+    '''          
+    def __init__(self, url: str="https://www.boxofficemojo.com/", driverpath: str='/Applications/chromedriver'): 
+        '''
+        The function initialises the class and takes in a url and a driverpath. 
+        The options and service object creates a driver object that is used to get the url.
+        The function sleeps for 3 seconds to allow the webpage to fully load.
+        
+        Parameters
+        ----------
+        url : str, optional
+            The url of the website you want to scrape.
+        driverpath : str, optional
+            This is the path to the chromedriver executable.
+        
+        '''    
         self.service = Service(driverpath)
         self.options = Options()
         self.driver = webdriver.Chrome(options=self.options, service=self.service) 
@@ -24,18 +54,40 @@ class Web_link_scraper:
         time.sleep(3)
 
     def _click_monthly_button(self):
+        '''
+        The function clicks the monthly button on the web home page.
+
+        '''
         domestic_container = self.driver.find_element(by=By.XPATH, value='//*[@id="a-page"]/div[2]/div[4]/div') 
         monthly_button = domestic_container.find_element(by=By.XPATH, value='//*[@id="a-page"]/div[2]/div[4]/div/a[4]') 
         monthly_button.click()
         time.sleep(3)
 
     def _select_year_from_scroll_down_menu(self):
+        '''
+        The function selects the year from the drop down menu.
+        
+        '''
         drop_down_list = self.driver.find_element(by=By.XPATH, value='//select[@id="view-navSelector"]') 
         select = Select(drop_down_list)
         select.select_by_visible_text('By year')
         time.sleep(3)
 
-    def _create_list_of_movie_links(self, year_list):    
+    def _create_list_of_movie_links(self, year_list):
+        '''
+        This function takes a list of years as an argument, and then loops through each 
+        year to get a list of web links corresponding to each movie.
+        
+        Parameters
+        ----------
+        year_list
+            a list of years that you want to scrape
+        
+        Returns
+        -------
+            A list of movie links.
+        
+        '''    
         self._select_year_from_scroll_down_menu()
         for year in (year_list): 
             drop_down_by_year = self.driver.find_element(by=By.XPATH, value='//select[@id="by-year-navSelector"]')
